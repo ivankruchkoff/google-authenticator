f3ae849cd40b2ac71674209402deb1962421740e
tammelin <tmmli@icloud.com>
2023-10-31 22:10:43 +0100

    Refill usernamd and password, focus on OTP input

--- google-authenticator.php
+++ google-authenticator.php
@@ -563,6 +563,29 @@ function loginfooter() {
 }
 
 /**
+ * Refill username and password fields on login form.
+ */
+function refillfields() {
+	echo "\n<script type=\"text/javascript\">\n";
+	echo "\ttry{\n";
+	echo "\t\tdocument.getElementById('user_login').value = '" . esc_js( $_POST['log'] ) . "';\n";
+	echo "\t\tdocument.getElementById('user_pass').value = '" . esc_js( $_POST['pwd'] ) . "';\n";
+	echo "\t} catch(e){}\n";
+	echo "</script>\n";
+}
+
+/**
+ * Focus on Google Authenticator code input field.
+ */
+function focusongoogleotp() {
+	echo "\n<script type=\"text/javascript\">\n";
+	echo "\ttry{\n";
+	echo "\t\tdocument.getElementById('googleotp').focus();\n";
+	echo "\t} catch(e){}\n";
+	echo "</script>\n";
+}
+
+/**
  * Login form handling.
  * Check Google Authenticator verification code, if user has been setup to do so.
  * @param wordpressuser / WP_Error
@@ -625,6 +648,10 @@ function check_otp( $user, $username = '', $password = '' ) {
 				} 		 
 			} else {
 				if ( ! $this->is_two_screen_signin_enabled() ) {
+					// Disable WP's autofocus on the username field
+					add_filter( 'enable_login_autofocus', '__return_false' );
+					add_action( 'login_footer', array( $this, 'refillfields' ) );
+					add_action( 'login_footer', array( $this, 'focusongoogleotp' ) );
 					return new WP_Error( 'invalid_google_authenticator_token', __( '<strong>ERROR</strong>: The Google Authenticator code is incorrect or has expired.', 'google-authenticator' ) );
 				} else {
 					wp_logout();
